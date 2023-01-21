0410a444dcf09cf279403e8e6d44524a9bf4656f
Amirhosein Gharaati <amirgh1380@gmail.com>
2023-01-21 18:00:20 +0330

    Uncomment main function checking arguments

--- main.c
+++ main.c
@@ -5,13 +5,13 @@
 int main(int argc, char **argv)
 {
 
-    // if (!check_arguments(argc))
-    // {
-    //     fprintf(stderr, "Not enough parameters\n");
-    //     fprintf(stderr, "Expected: -a SORTTYPE -n ARRAYSIZE -s ARRAYSTATE [-P]\n");
-    //     fprintf(stderr, "Please read the doc.\n");
-    //     return 1;
-    // }
+    if (!check_arguments(argc))
+    {
+        fprintf(stderr, "Not enough parameters\n");
+        fprintf(stderr, "Expected: -a SORTTYPE -n ARRAYSIZE -s ARRAYSTATE [-P]\n");
+        fprintf(stderr, "Please read the doc.\n");
+        return 1;
+    }
 
     Options *options = (struct opt *)malloc(sizeof(Options));
 
