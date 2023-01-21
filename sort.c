b149a309b6e9724ef4c84dad43c6ba5cd3d454fc
Amirhosein Gharaati <amirgh1380@gmail.com>
2023-01-21 17:50:59 +0330

    Implement sort_array function and add some variables

--- sort.c
+++ sort.c
@@ -1,5 +1,38 @@
 #include "sort.h"
+#include <string.h>
+#include <stdlib.h>
 
+clock_t start, end;
+double elapsed_time;
+// int numberOfComparisons;
+// int numberOfSwaps;
+
+void merge_sort(int *array, int number_of_elements)
+{
+}
+
+/**
+ * Method that receives a pointer to an array that will be sorted,
+ * his size and the enum of the method that will be used
+ * @param  array  Array to be sorted
+ * @param  size   Size of the array
+ * @param  method Sorting algorithm enum
+ * @return        Pointer to the sorted array
+ */
 int *sort_array(int *array, int size, enum SortMethod method)
 {
+    switch (method)
+    {
+    case MERGE:
+        start = clock();
+        merge_sort(array, size);
+        end = clock();
+        break;
+
+    default:
+        break;
+    }
+
+    elapsed_time = (((double)(end - start)) / CLOCKS_PER_SEC);
+    return array;
 }
--- sort.h
+++ sort.h
@@ -1,11 +1,18 @@
 #ifndef SORT
 #define SORT
 
+#include <time.h>
+
 enum SortMethod
 {
     MERGE = 1,
     UNDEFINED = -1
 };
+
+// extern int numberOfComparisons;
+// extern int numberOfSwaps;
+extern clock_t start, end;
+extern double elapsed_time;
 
 int *sort_array(int *array, int size, enum SortMethod method);
 
