465aa4539dac0bc763f32237590fc6b51f309b6a
Amirhosein Gharaati <amirgh1380@gmail.com>
2023-01-23 23:45:33 +0330

    Remove assert function

--- cuda_sort.cu
+++ cuda_sort.cu
@@ -15,7 +15,6 @@ inline cudaError_t checkCuda(cudaError_t result)
     if (result != cudaSuccess)
     {
         fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
-        assert(result == cudaSuccess);
     }
     return result;
 }
